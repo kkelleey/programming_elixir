91a32f5814e0ab12be8d011b5b10035eb852b894
kkelleey <karenkelley11@gmail.com>
2015-11-29 20:25:26 -0600

    adding ListsAndRecursion-8

--- ListsAndRecursion/mylist.exs
+++ ListsAndRecursion/mylist.exs
@@ -94,4 +94,24 @@ defmodule MyList do
     non_primes = for x <- span(2,n), y <- span(2,n), x > y, rem(x, y) == 0, do: x
     span(2,n) -- non_primes
   end
+
+  # Exercise: ListsAndRecursion-8
+  # tax_rates = [ NC: 0.075, TX: 0.08]
+  # orders = [
+  #   [id: 123, ship_to: :NC, net_amount: 100.00 ],
+  #   [ id: 124, ship_to: :OK, net_amount: 35.50 ],
+  #   [ id: 125, ship_to: :TX, net_amount: 24.00 ],
+  #   [ id: 126, ship_to: :TX, net_amount: 44.80 ],
+  #   [ id: 127, ship_to: :NC, net_amount: 25.00 ],
+  #   [ id: 128, ship_to: :MA, net_amount: 10.00 ],
+  #   [ id: 129, ship_to: :CA, net_amount: 102.00 ],
+  #   [ id: 130, ship_to: :NC, net_amount: 50.00 ] ]
+
+  def orders_with_total_amt(orders, tax_rates) do
+    for order <- orders do
+      tax_rate = Dict.get(tax_rates, order[:ship_to], 0)
+      total_amount = order[:net_amount] * ( 1 + tax_rate )
+      Dict.put_new(order, :total_amount, total_amount)
+    end
+  end
 end
