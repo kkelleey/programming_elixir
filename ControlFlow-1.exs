92316fc79c5da46568df6c7450b94579837d6b82
kkelleey <karenkelley11@gmail.com>
2015-12-05 15:34:45 -0500

    adding solution to ControlFlow-1.exs

---
+++ ControlFlow-1.exs
@@ -0,0 +1,14 @@
+defmodule FizzBuzz do
+  def upto(n) when n > 0 do
+    1..n |> Enum.map(&fizzbuzz/1)
+  end
+
+  defp fizzbuzz(n) do
+    case { rem(n, 3), rem(n, 5) } do
+      { 0, 0 } -> "FizzBuzz"
+      { 0, _ } -> "Fizz"
+      { _, 0 } -> "Buzz"
+      _ -> n
+    end
+  end
+end
