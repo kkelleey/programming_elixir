532069fd89e6fc10e6f690265c6a9d73b0a12bc0
kkelleey <karenkelley11@gmail.com>
2015-12-06 18:36:08 -0500

    adding WorkingWithMultipleProcesses-3 and 4

---
+++ WorkingWithMultipleProcesses-3.exs
@@ -0,0 +1,21 @@
+defmodule Link do
+  import :timer, only: [ sleep: 1 ]
+
+  def process do
+    exit(:message_sent_by_process)
+  end
+
+  def run do
+    Process.flag(:trap_exit, true)
+    spawn_link(Link, :process, [])
+    sleep 500
+    receive do
+      msg -> IO.puts "MESSAGE RECEIVED: #{inspect msg}"
+    end
+  end
+end
+
+Link.run
+
+# it doesn't matter that we weren't waiting for the notification from the child
+# when it exited
---
+++ WorkingWithMultipleProcesses-4.exs
@@ -0,0 +1,21 @@
+defmodule Link do
+  import :timer, only: [ sleep: 1 ]
+
+  def process do
+    raise "An error occured in the process"
+  end
+
+  def run do
+    Process.flag(:trap_exit, true)
+    spawn_link(Link, :process, [])
+    sleep 500
+    receive do
+      msg -> IO.puts "MESSAGE RECEIVED: #{inspect msg}"
+    end
+  end
+end
+
+Link.run
+
+# The full error information was passed to the parent instead of just the
+# message
