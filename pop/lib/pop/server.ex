8d5a77f23f700e871ef61306d0db75cbd07b0152
kkelleey <karenkelley11@gmail.com>
2015-12-13 16:01:11 -0500

    Adding external API and terminate function

--- pop/lib/pop/server.ex
+++ pop/lib/pop/server.ex
@@ -1,11 +1,30 @@
 defmodule Pop.Server do
   use GenServer
 
+  ###
+  # External API
+
+  def start_link(current_list) do
+    GenServer.start_link(__MODULE__, current_list, name: __MODULE__)
+  end
+
+  def pop do
+    GenServer.call __MODULE__, :pop
+  end
+
+  def push(value) do
+    GenServer.cast __MODULE__, { :push, value }
+  end
+
   def handle_call(:pop, _from, [head | tail]) do
     { :reply, head, tail }
   end
 
   def handle_cast({:push, value}, stack) do
     {:noreply, [ value | stack] }
+  end
+
+  def terminate(reason, state) do
+    IO.puts "#{reason} #{state}"
   end
 end
